c2a9fc1b97e5f1eb001b371f3c7c9559efe6b8f9
Kenneth Shepherd <kshepherd@pcs-publishing.com>
2022-01-06 08:09:35 +0000

    Added atomic control at the task level mainly to aid the use of traefik in my own projects.

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # Automatic public DNS for Fargate-managed containers in Amazon ECS
 
-Fargate-managed containers in ECS lack build-in support for registering services into public DNS namespaces (12/2018). This is an event-driven approach to automatically register the public IP of a deployed container in a Route 53 hosted zone.
+Fargate-managed containers in ECS lack build-in support for registering services
+into public DNS namespaces (12/2018). This is an event-driven approach to
+automatically register the public IP of a deployed container in a Route 53
+hosted zone.
 
-See [this blog post](https://medium.com/@andreas.pasch/automatic-public-dns-for-fargate-managed-containers-in-amazon-ecs-f0ca0a0334b5) for more information.
+See
+[this blog post](https://medium.com/@andreas.pasch/automatic-public-dns-for-fargate-managed-containers-in-amazon-ecs-f0ca0a0334b5)
+for more information.
 
 ## How it works
 
-A lambda function subscribes to an "ECS Task State Change" event. It gets called whenever a container has started up. What the function does is :
+A lambda function subscribes to an "ECS Task State Change" event. It gets called
+whenever a container has started up. What the function does is :
 
-* fetching the public IP from the container
-* construct a subdomain for the container
-* register the public IP for the subdomain in Route 53
+- fetching the public IP from the container
+- construct a subdomain for the container
+- register the public IP for the subdomain in Route 53
 
 ## Installation
 
@@ -18,7 +24,7 @@ First you need to pull NodeJs dependencies using:
 
 `npm install`
 
-You need to have the *Serverless Framework* CLI installed. 
+You need to have the _Serverless Framework_ CLI installed.
 
 Deploy the function in your active AWS account:
 
@@ -26,13 +32,22 @@ Deploy the function in your active AWS account:
 serverless deploy
 ```
 
-
 In your ECS console, select your cluster and add the tags
 
-* hostedZoneId (the hosted zone id of your public DNS namespace, for example `Z1OAI7EUAR14MP`)
-* domain (the domain name of your public DNS namespace, for example `foby.org`)
+- hostedZoneId (the hosted zone id of your public DNS namespace, for example
+  `Z1OAI7EUAR14MP`)
+- domain (the domain name of your public DNS namespace, for example `foby.org`)
+
+In your ECS console, create a new task definition and add the tags
+
+- setWildcard (true or false. True adds a wildcard domain for the task)
+- setDNS (true or false. True adds a DNS entry, otherwise it isn't set. You
+  might not always want a task to have a DNS entry.)
+- dnsPrefix (this sets the subdomain so setting "subdomain" would get you
+  subdomain.example.com. If it isn't set, the task name is used)
 
 ## Demo
 
-Well, just start a Fargate task in your cluster. When the task has started up, the function creates an A-record-set in your hosted zone with the containers' service name as subdomain.
-
+Well, just start a Fargate task in your cluster. When the task has started up,
+the function creates an A-record-set in your hosted zone with the containers'
+service name as subdomain.
--- src/update-task-dns.js
+++ src/update-task-dns.js
@@ -1,10 +1,10 @@
-'use strict';
+'use strict'
 
 const _ = require('lodash')
-const AWS = require('aws-sdk');
-const ec2 = new AWS.EC2();
-const ecs = new AWS.ECS();
-const route53 = new AWS.Route53();
+const AWS = require('aws-sdk')
+const ec2 = new AWS.EC2()
+const ecs = new AWS.ECS()
+const route53 = new AWS.Route53()
 
 /**
  * Upsert a public ip DNS record for the incoming task.
@@ -12,65 +12,115 @@ const route53 = new AWS.Route53();
  * @param event contains the task in the 'detail' propery
  */
 exports.handler = async (event, context, callback) => {
-    console.log('Received event: %j', event);
+  console.log('Received event: %j', event)
 
-    const task = event.detail;
-    const clusterArn = task.clusterArn;
-    console.log(`clusterArn: ${clusterArn}`)
+  const task = event.detail
+  const clusterArn = task.clusterArn
+  console.log(`clusterArn: ${clusterArn}`)
 
-    const clusterName = clusterArn.split(':cluster/')[1];
+  const clusterName = clusterArn.split(':cluster/')[1]
 
-    const tags = await fetchClusterTags(clusterArn)
-    const domain = tags['domain']
-    const hostedZoneId = tags['hostedZoneId']
+  const tags = await fetchClusterTags(clusterArn)
+  const domain = tags['domain']
+  const hostedZoneId = tags['hostedZoneId']
 
-    console.log(`cluster: ${clusterName}, domain: ${domain}, hostedZone: ${hostedZoneId}`)
+  console.log(
+    `cluster: ${clusterName}, domain: ${domain}, hostedZone: ${hostedZoneId}`
+  )
 
-    if (!domain || !hostedZoneId) {
-        console.log(`Skipping. Reason: no "domain" and/or "hostedZoneId" tags found for cluster ${clusterArn}`);
-        return;
-    }
-    
-    const eniId = getEniId(task);
-    if (!eniId) {
-        console.log('Network interface not found');
-        return;
-    }
+  if (!domain || !hostedZoneId) {
+    console.log(
+      `Skipping. Reason: no "domain" and/or "hostedZoneId" tags found for cluster ${clusterArn}`
+    )
+    return
+  }
 
-    const taskPublicIp = await fetchEniPublicIp(eniId)
-    const serviceName = task.group.split(":")[1]
-    console.log(`task:${serviceName} public-id: ${taskPublicIp}`)
+  const taskDeftags = await fetchTaskDefinitionTags(task.taskDefinitionArn)
+  console.log(`taskDefTags[${task.taskDefinitionArn}]: - ${taskDeftags}`)
+  const setWildcard = taskDeftags['setWildcard'] === 'true'
+  const setDNS = taskDeftags['setDNS'] === 'true'
+  const dnsPrefix = taskDeftags['dnsPrefix']
 
-    const containerDomain = `${serviceName}.${domain}`
-    const recordSet = createRecordSet(containerDomain, taskPublicIp)
+  if (setDNS === false) {
+    return
+  }
 
-    await updateDnsRecord(clusterName, hostedZoneId, recordSet)
-    console.log(`DNS record update finished for ${containerDomain} (${taskPublicIp})`)
-};
+  const eniId = getEniId(task)
+  if (!eniId) {
+    console.log('Network interface not found')
+    return
+  }
+
+  const taskPublicIp = await fetchEniPublicIp(eniId)
+  const serviceName = dnsPrefix || task.group.split(':')[1]
+  console.log(`task:${serviceName} public-id: ${taskPublicIp}`)
+
+  const containerDomain = `${serviceName}.${domain}`
+  const recordSet = createRecordSet(containerDomain, taskPublicIp)
+
+  await updateDnsRecord(clusterName, hostedZoneId, recordSet)
+
+  if (setWildcard) {
+    const wildCardContainerDomain = `*.${serviceName}.${domain}`
+    const wildCardRecordSet = createRecordSet(
+      wildCardContainerDomain,
+      taskPublicIp
+    )
+
+    await updateDnsRecord(clusterName, hostedZoneId, wildCardRecordSet)
+  }
+
+  console.log(
+    `DNS record update finished for ${containerDomain} (${taskPublicIp})`
+  )
+}
 
 async function fetchClusterTags(clusterArn) {
-    const response = await ecs.listTagsForResource({
-        resourceArn: clusterArn
-    }).promise()
-    return _.reduce(response.tags, function(hash, tag) {
-        var key = tag['key'];
-        hash[key] = tag['value'];
-        return hash;
-      }, {});
+  const response = await ecs
+    .listTagsForResource({
+      resourceArn: clusterArn
+    })
+    .promise()
+  return _.reduce(
+    response.tags,
+    function (hash, tag) {
+      var key = tag['key']
+      hash[key] = tag['value']
+      return hash
+    },
+    {}
+  )
+}
+
+async function fetchTaskDefinitionTags(taskDefinitionArn) {
+  const response = await ecs
+    .listTagsForResource({
+      resourceArn: taskDefinitionArn
+    })
+    .promise()
+  return _.reduce(
+    response.tags,
+    function (hash, tag) {
+      var key = tag['key']
+      hash[key] = tag['value']
+      return hash
+    },
+    {}
+  )
 }
 
 function getEniId(task) {
-    return _.chain(task.attachments)
-    .filter(function(attachment) {
-        return attachment.type === 'eni'
+  return _.chain(task.attachments)
+    .filter(function (attachment) {
+      return attachment.type === 'eni'
     })
-    .map(function(eniAttachment) {
-        return _.chain(eniAttachment.details)
-        .filter(function(details) {
-            return details.name === 'networkInterfaceId'
+    .map(function (eniAttachment) {
+      return _.chain(eniAttachment.details)
+        .filter(function (details) {
+          return details.name === 'networkInterfaceId'
         })
-        .map(function(details) {
-            return details.value
+        .map(function (details) {
+          return details.value
         })
         .head()
         .value()
@@ -80,39 +130,39 @@ function getEniId(task) {
 }
 
 async function fetchEniPublicIp(eniId) {
-    const data = await ec2.describeNetworkInterfaces({
-        NetworkInterfaceIds: [
-            eniId
-        ]
-    }).promise();
+  const data = await ec2
+    .describeNetworkInterfaces({
+      NetworkInterfaceIds: [eniId]
+    })
+    .promise()
 
-    return data.NetworkInterfaces[0].PrivateIpAddresses[0].Association.PublicIp;
+  return data.NetworkInterfaces[0].PrivateIpAddresses[0].Association.PublicIp
 }
 
 function createRecordSet(domain, publicIp) {
-    return {
-        "Action": "UPSERT",
-        "ResourceRecordSet": {
-            "Name": domain,
-            "Type": "A",
-            "TTL": 180,
-            "ResourceRecords": [
-                {
-                    "Value": publicIp
-                }
-            ]
+  return {
+    Action: 'UPSERT',
+    ResourceRecordSet: {
+      Name: domain,
+      Type: 'A',
+      TTL: 180,
+      ResourceRecords: [
+        {
+          Value: publicIp
         }
+      ]
     }
+  }
 }
 
 async function updateDnsRecord(clusterName, hostedZoneId, changeRecordSet) {
-    let param = {
-        ChangeBatch: {
-            "Comment": `Auto generated Record for ECS Fargate cluster ${clusterName}`,
-            "Changes": [changeRecordSet]
-        },
-        HostedZoneId: hostedZoneId
-    };
-    const updateResult = await route53.changeResourceRecordSets(param).promise();
-    console.log('updateResult: %j', updateResult);
+  let param = {
+    ChangeBatch: {
+      Comment: `Auto generated Record for ECS Fargate cluster ${clusterName}`,
+      Changes: [changeRecordSet]
+    },
+    HostedZoneId: hostedZoneId
+  }
+  const updateResult = await route53.changeResourceRecordSets(param).promise()
+  console.log('updateResult: %j', updateResult)
 }
